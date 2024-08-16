1f53a1f9dbc879e8d419bab841c958e5a2c4ad4a
pipinovi4 <prustolivgra@gmail.com>
2024-08-16 20:44:29 +0300

    Refactored ResidualBlock to support generic processing layers. - Introduced template parameter D for flexible layer integration. - Added support for handling MultiHeadAttention with an optional mask. - Updated forward and backward methods to work with different processing layers. - Enhanced error handling for mask usage in MultiHeadAttention.
    Signed-off-by: pipinovi4 <prustolivgra@gmail.com>

--- include/ResidualBlock.h
+++ include/ResidualBlock.h
@@ -1,30 +1,27 @@
+// include/ResidualBlock.h
 #ifndef RESIDUALBLOCK_H
 #define RESIDUALBLOCK_H
 
-#pragma once
-#include "Tensor.h"
 #include "LayerNorm.h"
+#include "Tensor.h"
+#include "MultiHeadAttention.h"
+#include "PositionalWiseDenseLayer.h"
 
-template <typename T>
+template <typename T, typename D>
 class ResidualBlock {
 public:
-    // Constructor that initializes the ResidualBlock
-    explicit ResidualBlock(int d_model, float epsilon = 1e-6);
+    explicit ResidualBlock(int d_model, float epsilon, D process_layer);
 
-    // Forward pass: takes input tensor and returns the output after residual connection and normalization
-    Tensor<T> forward(const Tensor<T>& input, const Tensor<T>& processed);
+    Tensor<T> forward(const Tensor<T>& input, const Tensor<T>* mask = nullptr);
 
-    // Backward pass: calculates the gradients with respect to the input and processed tensors
     Tensor<T> backward(const Tensor<T>& dout);
 
 private:
-    // Layer normalization applied after adding the residual
     LayerNorm<T> layer_norm_;
-
-    // Intermediate tensors to store results needed for backpropagation
-    Tensor<T> input_;       // The original input tensor
-    Tensor<T> processed_;   // The processed tensor before adding the residual
-    Tensor<T> output_;      // The output tensor after forward pass
+    D process_layer_;
+    Tensor<T> input_;
+    Tensor<T> processed_;
+    Tensor<T> output_;
 };
 
 #include "../src/ResidualBlock.tpp"
