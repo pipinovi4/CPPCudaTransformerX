e20cd0142369d82afc6e3662f68e83aadab9d913
pipinovi4 <prustolivgra@gmail.com>
2024-09-27 05:40:20 +0300

    Wrote method for generation text from context tokens which must passed over cmd
    Signed-off-by: pipinovi4 <prustolivgra@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,117 +1,142 @@
 #include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <map>
-#include <string>
 #include <algorithm>
-#include <utility>
-#include <functional>
-#include "../include/Tokenizer.h"
-#include "../utils/loadAGNews.tpp"
-#include <deque>
-#include "../include/Embedding.h"
-#include "../include/Tensor.h"
+#include <string>
+#include <vector>
+#include <sstream>
+#include "../include/LossFunction.h"
+#include "../include/Optimizer.h"
 #include "../models/Transformer.h"
-#include "../utils/loadWikiText.tpp"
 #include "../utils/loadVocab.h"
-#include <chrono>  // Include the chrono library
 
 int main() {
-    // Load the AG News dataset
-    const std::vector<std::vector<std::vector<std::string>>> dataset = loadWikiText("data/wikitext");
+    int initial_token_count = 0;
+    std::string input_text;
+    std::vector<std::string> tokens;
 
-    // Get the train data
-    std::vector<std::vector<std::string>> train_data = dataset[0];
-    train_data.resize(1000);
+    std::cout << "Enter context for text generation (max 4 tokens): ";
+    std::getline(std::cin, input_text);
 
-    // Get the validation data
-    const std::vector<std::vector<std::string>> val_data = dataset[1];
-
-    // Get the test data
-    const std::vector<std::vector<std::string>> test_data = dataset[2];
-
-    // Load the vocabulary
-    const std::vector<std::string> vocab = load_vocab("data/vocab/vocab_30000_words.txt");
-
-    // Parameters
-    constexpr int max_tokens = 8;
-    constexpr int d_model = 16;
-    constexpr int num_heads = 2;
-    constexpr int d_ff = 32;
-    const int vocab_size = static_cast<int>(vocab.size());
-    constexpr float learning_rate = 0.001;
-    constexpr float decay_rate = 0.5;
-
-    // Define learning rate scheduler
-    Optimizer<float>::LearningRateSchedule::ExponentialDecaySchedule learning_rate_schedule(learning_rate, decay_rate);
-
-    // Define loss function
-    LossFunction<float>::crossEntropyLoss loss_fn;
-
-    // Define optimizer
-    Optimizer<float>::Adam optimizer(learning_rate_schedule);
-
-    // Define the Transformer model
-    Transformer<float> transformer(vocab_size, d_model, num_heads, d_ff, max_tokens, decay_rate, decay_rate, 2, learning_rate_schedule, &loss_fn, &optimizer, vocab);
-
-    // Convert to tensor
-    auto train_data_tensor = transformer.convert_to_tensor(val_data);
-
-    auto src = train_data_tensor[0];
-    auto tgt = train_data_tensor[1];
-    auto true_labels = train_data_tensor[2];
-
-    transformer.load_weights("data/transformer_models/transformer_weights.txt");
-
-    // Train the model
-    auto start = std::chrono::high_resolution_clock::now();
-    transformer.train(train_data, 1, 128);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end - start;
-    std::cout << "Time: " << diff.count() << " s\n";
-
-
-    // Print parameters
-    for (auto param : transformer.parameters()) {
-        param.get().print();
+    // Split input into individual tokens
+    std::istringstream stream(input_text);
+    std::string word;
+    while (stream >> word) {
+        tokens.push_back(word);
     }
 
-    // Predict
-    transformer.predict({{"How", "are", "you", "?"}}, 8);
+    initial_token_count = tokens.size();
 
+    // Adjust token size if greater than 4
+    if (tokens.size() > 4) {
+        std::cout << "Maximum context size is 4 tokens." << std::endl;
+        tokens.resize(4);
+        input_text = "";
+        for (const auto& token : tokens) {
+            input_text += token + " ";
+        }
+
+        std::cout << "Reduced to 4 tokens: " << input_text << std::endl;
+        std::cout << "Continue? (yes/no): ";
+
+        std::string response;
+        std::getline(std::cin, response);
+        std::transform(response.begin(), response.end(), response.begin(), ::tolower);
+
+        if (response == "no" || response == "n") {
+            std::cout << "Exiting..." << std::endl;
+            return 0;
+        }
+    } else if (tokens.size() < 4) {
+        std::cout << "Context has fewer than 4 tokens." << std::endl;
+        int missing_tokens = 4 - tokens.size();
+        tokens.insert(tokens.end(), missing_tokens, "<PAD>");
+        input_text = "";
+        for (const auto& token : tokens) {
+            input_text += token + " ";
+        }
+
+        std::cout << "Filled with <PAD>: " << input_text << std::endl;
+        std::cout << "Continue? (yes/no): ";
+
+        std::string response;
+        std::getline(std::cin, response);
+        std::transform(response.begin(), response.end(), response.begin(), ::tolower);
+        if (response == "no" || response == "n") {
+            std::cout << "Exiting..." << std::endl;
+            return 0;
+        }
+    }
+
+    // Load vocabulary
+    std::cout << "Loading vocabulary..." << std::endl;
+    const std::vector<std::string> vocab = load_vocab("../data/vocab/vocab_30000_words.txt");
+    std::cout << "Vocabulary loaded." << std::endl;
+
+    // Model parameters
+    constexpr int max_tokens = 8;
+    constexpr int d_model = 32;
+    constexpr int n_heads = 8;
+    constexpr int d_ff = 128;
+    const int vocab_size = static_cast<int>(vocab.size());
+    constexpr float learning_rate = 0.001;
+    constexpr float decay_rate = 0.9;
+    constexpr float weight_decay = 0.001;
+    constexpr float b1 = 0.9;
+    constexpr float b2 = 0.999;
+    constexpr float epsilon = 1e-8;
+    constexpr float label_smoothing = 0.1;
+    constexpr float dropout = 0.1;
+    const std::string weights_path = "../data/weights/transformer_weights.txt";
+
+    // Initialize learning rate scheduler, loss function, and optimizer
+    Optimizer<float>::LearningRateSchedule::ExponentialDecaySchedule lr_schedule(learning_rate, decay_rate);
+    LossFunction<float>::crossEntropyLoss loss_fn;
+    Optimizer<float>::Adam optimizer(learning_rate, lr_schedule, weight_decay, b1, b2, epsilon);
+
+    // Initialize and load the Transformer model
+    Transformer<float> transformer(&loss_fn, &optimizer, vocab, lr_schedule, vocab_size, d_model, n_heads, d_ff, max_tokens, dropout, label_smoothing);
+    transformer.load_weights(weights_path);
+
+    std::cout << "Do you want to see the model's parameters? (yes/no): ";
+    std::string response;
+    std::getline(std::cin, response);
+    std::transform(response.begin(), response.end(), response.begin(), ::tolower);
+
+    if (response == "yes" || response == "y") {
+        std::cout << "\nModel Parameters:\n";
+        std::cout << "Vocab size: " << vocab_size << "\n"
+                  << "Max tokens: " << max_tokens << "\n"
+                  << "Learning rate: " << learning_rate << "\n"
+                  << "d_model: " << d_model << "\n"
+                  << "n_heads: " << n_heads << "\n"
+                  << "d_ff: " << d_ff << "\n"
+                  << "Weight decay: " << weight_decay << "\n"
+                  << "b1: " << b1 << "\n"
+                  << "b2: " << b2 << "\n"
+                  << "Epsilon: " << epsilon << "\n"
+                  << "Dropout: " << dropout << "\n"
+                  << "Label smoothing: " << label_smoothing << "\n";
+    }
+
+    // Generate text based on input
+    const std::vector<std::vector<std::string>> generated_text = transformer.generate({transformer.positional_encoder_->tokenize(input_text)}, initial_token_count);
+    std::cout << "Text generated successfully!" << std::endl;
+
+    // Output the generated text
+    std::cout << "\nGenerated text: ";
+    for (int i = initial_token_count + 1; i < generated_text[0].size(); ++i) {
+        std::cout << generated_text[0][i] << " ";
+    }
+
+    std::cout << "\nFull generated text: ";
+    for (const auto& sentence : generated_text) {
+        for (const auto& word : sentence) {
+            std::cout << word << " ";
+            if (word == "<EOS>") break;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Goodbye!" << std::endl;
     return 0;
 }
-
-
-// // Activation function relu
-// ActivationFunction<float>::ReLU relu;
-// // Learning rate scheduler
-// Optimizer<float>::LearningRateSchedule::ExponentialDecaySchedule learning_rate_schedule(0.001, 0.5);
-// // Embeddding layer
-// Embedding<float> embedding(30000, 512, learning_rate_schedule);
-//
-// // Dense layer
-// DenseLayer<float> dense(128, 30000, &relu, 0.0);
-//
-// // Position-wise dense layer
-// PositionalWiseDenseLayer<float> pos_dense(512, 128, relu);
-//
-// // Multi-head attention
-// MultiHeadAttention<float> multi_head_attention(128, 8, 64, &relu);
-//
-// // Residual block
-// ResidualBlock<float, PositionalWiseDenseLayer<float>*> residual_block(128, .95, &pos_dense);
-//
-// // Forward pass layers for testing in place operations
-// Tensor<float> input = Tensor<float>::uniform({128}, 0, 1);
-//
-// input = embedding.forward(input);
-// auto input2 = input;
-//
-// // // Pass Dense layer
-// // // Take a time
-// auto start = std::chrono::high_resolution_clock::now();
-// pos_dense.forward(input);
-// auto end = std::chrono::high_resolution_clock::now();
-// std::chrono::duration<double> diff = end - start;
-// std::cout << "Time eigen: " << diff.count() << " s\n";
